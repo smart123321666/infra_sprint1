b40207c54d7a78f98a96cdf843ec0c20dc55c99e
smart123321666 <scake14@yandex.ru>
2024-03-11 12:53:09 +0000

    dsa

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,10 +4,12 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 
-DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = False
+
+ALLOWED_HOSTS = ['158.160.68.51','127.0.0.1','localhost','yandexinfra.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -91,9 +93,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / 'static_backend'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/var/www/kittygram/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
